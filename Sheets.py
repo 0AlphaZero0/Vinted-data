2143d50274c6ea8bf5eb63c43c5ef5feb6f41c16
AlphaZero <the-prodigy-moi@live.fr>
2021-05-01 12:08:47 +0200

    Initial commit

---
+++ Sheets.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 18 23:18:00 2021
+
+@author: Arthur Thouvenin
+"""
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import os
+import sys
+import pandas as pd
+import json
+from datetime import datetime
+
+
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/spreadsheets",
+         "https://www.googleapis.com/auth/drive.file",
+         "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+spreadsheet = client.open("Articles")
+spreadsheetPublic = client.open("Vinted_Data")
+
+sheet_Articles = spreadsheet.worksheet("Articles")
+sheet_Chiffres = spreadsheet.worksheet("Chiffres")
+
+
+
+
+def updateVintedSheets(reload_data=False):
+
+    def unPack_Catalogs(json_f):
+
+        def recursive_Catalog(json_obj):
+            field_lists = ["photo","material_group_ids","size_group_ids","package_size_ids"]
+            DATA = []     
+            for catalog in json_obj:
+                for field in field_lists:
+                    if field in catalog:
+                        del catalog[field]
+                if len(catalog["catalogs"])!=0:
+                    unpack = recursive_Catalog(catalog["catalogs"])
+                    DATA = DATA + unpack
+                del catalog["catalogs"]
+                DATA.append(catalog)
+            return DATA
+
+        json_f = json.load(json_f)
+        DATA = pd.json_normalize(recursive_Catalog(json_f))
+        return DATA
+
+    def unPack_Materials(json_f):
+        json_f = json.load(json_f)
+        DATA = []
+        for material in json_f:
+            title = material["title"]
+            for mat in material["materials"]:
+                mat["parent"] = title
+                DATA.append(mat)
+            del material["materials"]
+            material["parent"] = title
+            DATA.append(material)
+        DATA = pd.json_normalize(DATA)
+        return DATA
+
+    def unPack_Sizes(json_f):
+        json_f = json.load(json_f)
+        field_lists = [
+            "size_ids"]
+        DATA = []
+        for size in json_f:
+            for field in field_lists:
+                if field in size:
+                    del size[field]
+            parent_title = size["description"]
+            parent_id = size["id"]
+            if len(size["sizes"]) != 0:
+                for s in size["sizes"]:
+                    s["parent_id"] = parent_id
+                    s["parent_title"] = parent_title
+                    DATA.append(s)
+            del size["sizes"]
+            DATA.append(size)
+        DATA = pd.json_normalize(DATA)
+        return DATA
+
+    def unPack_Countries(json_f):
+        json_f = json.load(json_f)
+        field_lists = ["postal_code_constraints"]
+        for item in json_f:
+            for field in field_lists:
+                if field in item:
+                    del item[field]
+        return pd.json_normalize(json_f)
+
+    def readJSON(json_f):
+        return pd.read_json(json_f)
+
+    id_files = {
+        "Brand":{
+            'function':readJSON
+            },
+        "Catalog":{
+            'function':unPack_Catalogs
+            },
+        "Color":{
+            'function':readJSON
+            },
+        "Country":{
+            'function':unPack_Countries
+            },
+        "Material":{
+            'function':unPack_Materials
+            },
+        "Size":{
+            'function':unPack_Sizes
+            },
+        "Status":{
+            'function':readJSON
+            }
+        }
+
+    folder = "./DATA/"
+    if "JSONfromID" not in sys.modules:
+        from collect_data import JSONfromID
+    if reload_data:
+        JSONfromID(id_range=range(0,10000),per_page=110,save=True)
+    for file in os.listdir(folder):
+        file_name = file.split(".")[0].capitalize()
+        print(file_name)
+        with open(folder+file,"r") as json_file:
+            data = id_files[file_name]['function'](json_file)
+        data.fillna('', inplace=True)
+        sheet = spreadsheet.worksheet(file_name)
+        sheet.update([data.columns.values.tolist()] + data.values.tolist())
+        public_sheet = spreadsheetPublic.worksheet(file_name)
+        public_sheet.update([data.columns.values.tolist()] + data.values.tolist())
+    now = datetime.now()
+    public_sheet = spreadsheetPublic.worksheet("Vinted_Summary")
+    public_sheet.update('E12',str(now.strftime("%d-%m-%Y %H:%M")))
+    return
+
+
+
+
+if __name__ == '__main__':
+    updateVintedSheets()
---
+++ collect_data.py
@@ -0,0 +1,278 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 12 11:45:45 2021
+
+@author: Arthur Thouvenin
+"""
+
+import requests
+import json
+import re
+
+api_url = "https://www.vinted.fr/api/v2/items?search_text=&catalog_ids=&color_ids=&brand_ids=&size_ids=&material_ids=&status_ids=&country_ids=&city_ids=&is_for_swap=0&page=1&per_page="
+url = "https://www.vinted.fr/vetements?search_text=&brand_id[]=&color_id[]="
+
+
+def JSONfromID(id_names=["catalog","color","brand","size","material","status","country"],id_range=range(0,100),per_page=24,save=False,empty_ids=False):
+    """
+    This function will extract information from Vinted about the ids requested.
+
+    Parameters
+    ----------
+    id_names : LIST or STRING, optional
+        This parameter indicate which ids you want to extract from Vinted. The default is ["catalog","color","brand","size","material","status","country"].
+    id_range : RANGE, optional
+        This parameter is used to extract a range of ids for example from 0 to 1000. This parameter is only used to extract brand ids. The default is range(0,100).
+    per_page : INTEGER, optional
+        This parameter is the number of item per page in the resulting response the best to extract ids corresponds to 24. The default is 24.
+    save : BOOLEAN, optional
+        This parameter is used to saved ids found in the DATA folder, as following : ./DATA/*id_name*.json. The default is False.
+    empty_ids : BOOLEAN, optional
+        This parameter is used only for the brand id results. Indeed some brands ids point to nothing, if you want those empty ids in your results set this parameter to True. The default is False.
+
+    Returns
+    -------
+    collected_data : DICTIONARY
+        This dictrionary will contains every ids found. For example if the id_names parameter was ["color","status"] the dictionary will look like this :
+            {
+                "color":[
+                              {
+                                "id": 1,
+                                "title": "Noir",
+                                "hex": "000000",
+                                "order": 1,
+                                "code": "BLACK"
+                              },
+                              {
+                                "id": 3,
+                                "title": "Gris",
+                                "hex": "919191",
+                                "order": 2,
+                                "code": "GREY"
+                              },
+                              .
+                              .
+                              .
+                        ],
+                "status":[
+                              {
+                                "id": 6,
+                                "title": "Neuf avec \u00e9tiquette",
+                                "description": "Article neuf, jamais port\u00e9/utilis\u00e9 avec \u00e9tiquettes ou dans son emballage d\u2019origine.",
+                                "explanation": "Article neuf, jamais port\u00e9/utilis\u00e9 avec \u00e9tiquettes ou dans son emballage d\u2019origine.",
+                                "explanation_title": "Cet article est flambant neuf ?",
+                                "is_default": 0,
+                                "order": 5
+                              },
+                              {
+                                "id": 1,
+                                "title": "Neuf sans \u00e9tiquette",
+                                "description": "Article neuf, jamais port\u00e9/utilis\u00e9, sans \u00e9tiquettes ni emballage d\u2019origine.",
+                                "explanation": "Article neuf, jamais port\u00e9/utilis\u00e9, sans \u00e9tiquettes ni emballage d\u2019origine.",
+                                "explanation_title": "L\u2019article n\u2019a plus d\u2019\u00e9tiquette, mais il n\u2019a jamais \u00e9t\u00e9 port\u00e9 ?",
+                                "is_default": 0,
+                                "order": 10
+                              },
+                              .
+                              .
+                              .
+                        ]
+              }
+
+    """
+    
+    def brandIds(id_name,id_supported,id_range=id_range,per_page=per_page,empty_ids=empty_ids):
+        """
+        This function will extract a range of brand ids from Vinted and their corresponding information.
+
+        Parameters
+        ----------
+        id_name : STRING
+            This parameter is not used here but essential.
+        id_supported : DICTIONARY
+            This parameter is not used here but essential.
+        id_range : RANGE, optional
+            This parameter is used to extract a range of ids for example from 0 to 1000. This parameter is only used to extract brand ids. The default is range(0,100).
+        per_page : INTEGER, optional
+            This parameter is the number of item per page in the resulting response the best to extract ids corresponds to 24. The default is 24.
+        empty_ids : BOOLEAN, optional
+            This parameter is used only for the brand id results. Indeed some brands ids point to nothing, if you want those empty ids in your results set this parameter to True. The default is False.
+
+        Returns
+        -------
+        id_DATA : LIST
+            This list will contains every brand ids found and their corresponding information.
+
+        """
+        id_DATA=[]
+        for i in id_range:
+            new_url = url.replace(id_name+"_id[]=",id_name+"_id[]="+str(i))
+            req = requests.get(new_url).text
+            print("#######################\n"+id_name.split("_")[0].capitalize()+" ID Extraction...")
+            print(new_url)
+            print(id_name+" : "+str(i))
+            brands = re.findall(r"({\"id\":[0-9]+,\"title\":\"[^\"]+\",\"slug\":[^}]+})", req)
+            for pot_brand in brands:
+                pot_brand=json.loads(pot_brand)
+                if pot_brand["id"] == i:
+                    brand_info = pot_brand
+                    brand_title = brand_info["title"]
+                    id_DATA.append(brand_info)
+                    break
+            else:
+                brand_info = {"id":i, "brand_title":""}
+                brand_title = ""
+                if empty_ids:
+                    id_DATA.append(brand_info)
+            print(id_name.capitalize()+" Name Found : "+brand_title)
+        return id_DATA
+    
+    def params(id_supported,id_names=id_names):
+        """
+        This function will format the id_names as a list.
+
+        Parameters
+        ----------
+        id_names : STRING or LIST, optional
+            A list or string that should correpond to one of the supported Vinted IDs. The default is id_names.
+        id_supported : DICTIONARY
+            A dictionary containing supported Vinted IDs and their corresponding information
+
+        Returns
+        -------
+        id_names : LIST
+            A list of supported Vinted IDs.
+
+        """
+
+        def check_supported_ids(id_names=id_names,id_supported=id_supported):
+            """
+            This function will check if all ids in id_name are supported.
+
+            Parameters
+            ----------
+            id_names : list, optional
+                A list or string that should correpond to one of the supported Vinted IDs. The default is id_names.
+            id_supported : TYPE, optional
+                A dictionary containing supported Vinted IDs and their corresponding information. The default is id_supported.
+
+            Returns
+            -------
+            BOOLEAN
+            """
+            id_not_supported = []
+            for i in id_names:
+                if i not in id_supported:
+                    id_not_supported.append(i)
+            if len(id_not_supported) != 0:
+                raise(f'Following ids are not supported currently ({id_names}), please check supported ids.\n {str_id_supp}')
+            return True
+
+        if isinstance(id_names, str) and id_names in id_supported:
+            return [id_names]
+        if isinstance(id_names, list) and check_supported_ids(id_names,id_supported):
+            return id_names
+        str_id_supp = id_supported.keys()
+        raise(f'Following id is not supported currently ({id_names}), please check supported ids.\n {str_id_supp}')
+        
+    def regexMatching(id_name,id_supported):
+        """
+        This function will, from an ID name, extract the corresponding Python object from Vinted with a regex matching.
+
+        Parameters
+        ----------
+        id_name : STRING
+            A string corresponding to a supported Vinted ID.
+        id_supported : DICTIONARY
+            Dictionary which contains Vinted IDs and their corresponding information.
+
+        Returns
+        -------
+        id_DATA : JSON - PYTHON OBJECT
+            Python object corresponding to the JSON found in Vinted data corresponding to the id_name.
+
+        """
+        req = requests.get(url).text
+        id_DATA = json.loads(re.findall(id_supported[id_name]["regex"], req)[0])
+        return id_DATA
+
+    def colorModification(id_DATA):
+        """
+        This function will add the # to the hex field of the Python object corresponding to the ID color in Vinted.
+
+        Parameters
+        ----------
+        id_DATA : JSON - PYTHON OBJECT
+            The python object collected for the color ID.
+
+        Returns
+        -------
+        id_DATA : JSON - PYTHON OBJECT
+            The same python object collected for the color ID, but with the # in front of the hex values.
+
+        """
+        for color in id_DATA:
+            color["hex"] = "#"+color["hex"]
+        return id_DATA
+    
+    id_supported = {
+        "catalog":{
+            "function":regexMatching,
+            "regex":r"\"catalogFilters\":{\"dtos\":{\"catalogs\":(.*),\"colors"
+            },
+        "color":{
+            "function":regexMatching,
+            "regex":r"\"colors\":(.*),\"statuses",
+            "modification":colorModification
+            },
+        "brand":{
+            "function":brandIds
+            },
+        "size":{
+            "function":regexMatching,
+            "regex":r"\"sizeGroups\":(.*),\"materialGroups"
+            },
+        "material":{
+            "function":regexMatching,
+            "regex":r"\"materialGroups\":(.*),\"countries"
+            },
+        "status":{
+            "function":regexMatching,
+            "regex":r"\"statuses\":(.*),\"brands"
+            },
+        "country":{
+            "function":regexMatching,
+            "regex":r"\"countries\":(.*),\"cities"
+            }
+        }
+
+
+    id_names = params(id_supported)
+    collected_data = {}
+    for id_name in id_names:
+        id_DATA = id_supported[id_name]["function"](id_name,id_supported)
+        if "modification" in id_supported[id_name]:
+            id_DATA = id_supported[id_name]["modification"](id_DATA)
+        collected_data[id_name]=id_DATA
+        print(id_name.capitalize()+" found : "+str(len(id_DATA)))
+
+    if save:
+        for id_collected in collected_data:
+            with open("./DATA/"+id_collected+".json",'w') as outfile:
+                json.dump(collected_data[id_collected],outfile,indent=2)
+    return collected_data
+
+def searchVinted(searchText="",catalog=[],color=[],brand=[],size=[],material=[],status=[],country=[],per_page=110,page=1):
+    params = locals()
+    url_search = "https://www.vinted.fr/vetements?search_text="+searchText
+    del params["searchText"],params["per_page"],params["page"]
+    for param in params:
+        if len(params[param]) != 0:
+            for param_id in params[param]:
+                url_search = url_search+"&"+param+"_id[]="+str(param_id)
+    url_search = url_search+"&per_page="+str(per_page)+"&page="+str(page)
+    req = requests.get(url_search).text
+    return req
+
+if __name__ == '__main__':
+    JSONfromID()
